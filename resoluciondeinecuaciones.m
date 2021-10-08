bfd5e5eff105e6fe19420633eb6515abb7b18dd9
03XimenaCruz <91585035+03XimenaCruz@users.noreply.github.com>
2021-10-07 22:55:26 -0500

    Add files via upload

---
+++ resoluciondeinecuaciones.m
@@ -0,0 +1,34 @@
+% Octave Script
+% Title               :Resolucion de inecuaciones
+% Description         :Script para recordar la resolucion de inecuaciones 
+% Autor               :Ximena Mayleth Cruz Cruz 
+% Date                :7/10/21
+% Version             :1
+% Usage               :octave> /path/Resoluciondeinecuaciones
+% Notes               :http://octaveintro.readthedocs.io/en/latest/index.html
+%
+
+clear 
+%Se llama inecuacion a una desigualdad que contiene variables cambiando el = por un <, >, >=, <=,;
+inecuaciones_primer_grado = ' Son aquellas que pueden expresar de la forma ax+b=0 , siendo a y b numeros reales y a?0';
+inecuaciones_segundo_grado  = ' Se pueden expresar de la siguiente forma ax^2+bx+c>0 ';
+inecuaciones_con_valor_absoluto = ' Se pueden expresar de la forma |x|<0 ';
+
+% Inecuacion de primer grado con una incognita
+i_primer_grado = ' ax+b<o ';
+syms x
+solve (x+6<0)
+
+% Inecuacion de segundo grado con una incognita
+i_segundo_grado = ' ax^2+bx+c>0 ';
+i_segundo_grado2 = ' ax^2+bx+c<0 ';
+i_segundo_grado3 = 'ax^2+bx+c<=0 ';
+i_segundo_grado4 = 'ax^2+bx+c>=0 ';
+syms x
+solve (x^2+3*x-2>0) 
+
+% Inecuacion con valor absoluto 
+i_con_valor_absoluto = ' |x|<0 ';
+    solve(abs(3*x-7)<=2,'returnconditions')
+    
+
